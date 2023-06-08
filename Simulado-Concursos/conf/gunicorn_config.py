0393ef32063d033f369ca9075c0a59aac6b1f5d3
ryck-developer <oliveirachaves97@gmail.com>
2023-06-08 09:25:28 -0300

    new

--- Simulado-Concursos/conf/gunicorn_config.py
+++ Simulado-Concursos/conf/gunicorn_config.py
@@ -1,4 +1,5 @@
 command = '/usr/local/bin/gunicorn'
 pythonpath = '/usr/local/bin/python3'
-bind = '191.96.251.30:8000'
-workers = 3
+bind = "0.0.0.0:8000"
+chdir = "/app/"
+module = "ryck.wsgi:application"
