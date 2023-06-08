6c068dcdbd68d3b76225b972867525b5e7a64dee
ryck-developer <oliveirachaves97@gmail.com>
2023-06-08 10:12:19 -0300

    new

--- Simulado-Concursos/conf/gunicorn_config.py
+++ Simulado-Concursos/conf/gunicorn_config.py
@@ -1,5 +1,11 @@
+import multiprocessing
+
 command = '/usr/local/bin/gunicorn'
 pythonpath = '/usr/local/bin/python3'
-bind = "0.0.0.0:8000"
+
+workers = multiprocessing.cpu_count() * 2 + 1
+worker_class = 'gthread'
+threads = 4
+
 chdir = "/app/"
 module = "ryck.wsgi:application"
